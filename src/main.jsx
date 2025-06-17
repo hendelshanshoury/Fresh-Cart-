c8c1a6cf068c2a85c25b9983ebc8f8a4c74ba4d9
Hend Elsayed <eng.hendelshanshoury@gmail.com>
2025-06-18 00:39:54 +0300

    Wrap <App /> component with <BrowserRouter> for routing support

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import "./index.css";
 import App from "./App.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import 'react-responsive-pagination/themes/minimal.css';
+import "react-responsive-pagination/themes/minimal.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
